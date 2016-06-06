82969f2773278782c8c8a5b5537d15fdffe697dc
Ariel Hernandez <ariel.h.estevenz@gmail.com>
2016-06-06 15:43:42 -0300

    Added: [apOCVPointGrey] apOCVPointGrey C++ Class.
    C++ Class to manage PointGrey cameras with OpenCV library.

---
+++ apOCVPointGrey.cpp
@@ -0,0 +1,166 @@
+#include "apOCVPointGrey.hpp"
+
+using namespace ap;
+
+OCVPointGrey::OCVPointGrey()
+    :camera(Camera_ptr(new Camera))
+{
+
+}
+
+OCVPointGrey::~OCVPointGrey()
+{
+
+}
+
+bool OCVPointGrey::OpenCamera()
+{
+    CameraInfo camInfo;   
+    BusManager busMgr;
+    unsigned int numCameras=0;
+
+    /* Number of cameras*/
+    if(!GetCamerasNumber(numCameras, &busMgr)) return false;
+    /* Get camera index */
+    if(!GetCamera(&busMgr)) return false;
+    /* Connect the camera */
+    if(!CameraConnect()) return false;
+    /* Get the camera info */
+    if(!GetCameraInfo(camInfo, false)) return false;
+    /* Start capture */
+    if(!StartCameraCapture()) return false;
+
+    return true;
+}
+
+bool OCVPointGrey::GetCamerasNumber(unsigned int num, BusManager *bus_mgr)
+{
+    Error error;
+    error=bus_mgr->GetNumOfCameras(&num);
+       if ( error != PGRERROR_OK )
+       {
+           cerr << "Failed to read the number of cameras" << endl;
+           return false;
+       }
+       if( num != 1 )
+       {
+           cerr << "Please connect only one camera: " << num << endl;
+           return false;
+       }
+   return true;
+}
+
+bool OCVPointGrey::GetCamera(FlyCapture2::BusManager *bus_mgr)
+{
+    Error error;
+    error = bus_mgr->GetCameraFromIndex(0, &guid);
+    if ( error != PGRERROR_OK )
+    {
+        cerr << "Failed to get the camera. Error code: "<< error.GetLine() << endl;
+        return false;
+    }
+
+    return true;
+}
+
+bool OCVPointGrey::CameraConnect()
+{
+    Error error;
+    error = camera->Connect(&guid );
+    if ( error != PGRERROR_OK )
+    {
+        cerr << "Failed to connect to the camera. Error code: "<< error.GetLine() << endl;
+        return false;
+    }
+
+    return true;
+}
+
+bool OCVPointGrey::GetCameraInfo(CameraInfo camInfo, bool show)
+{
+    Error error;
+    error = camera->GetCameraInfo( &camInfo );
+    if ( error != PGRERROR_OK )
+    {
+         cerr << "Failed to get information from the camera. Error code: "<< error.GetLine() << endl;
+         return false;
+    }
+    if (show)
+    {    cout << camInfo.vendorName << " "
+         << camInfo.modelName << " "
+         << camInfo.serialNumber << endl;
+    }
+
+    return true;
+}
+
+bool OCVPointGrey::StartCameraCapture()
+{
+    Error error;
+    error = camera->StartCapture();
+    if ( error == PGRERROR_ISOCH_BANDWIDTH_EXCEEDED )
+    {
+        cerr << "Bandwidth exceeded" << endl;
+        return false;
+    }
+    else if ( error != PGRERROR_OK )
+    {
+        cerr << "Failed to start image capture. Error code: "<< error.GetLine() << endl;
+        return false;
+    }
+
+    return true;
+}
+
+bool OCVPointGrey::StopCameraCapture()
+{
+    Error error;
+    error = camera->StopCapture();
+    if ( error != PGRERROR_OK )
+    {
+        cerr << "Failed to stop image capture. Error code: "<< error.GetLine() << endl;
+        return false;
+    }
+
+    return true;
+}
+
+bool OCVPointGrey::RestoreCameraBuffer()
+{
+    Error error;
+    error=camera->RetrieveBuffer( &rawImage );
+    if ( error != PGRERROR_OK )
+    {
+        cerr << "Capture error. Error code: "<< error.GetLine() << endl;
+        return false;
+    }
+
+    return true;
+}
+
+void OCVPointGrey::ConvertToRGB()
+{
+    rawImage.Convert( FlyCapture2::PIXEL_FORMAT_BGR, &rgbImage );
+}
+
+cv::Mat OCVPointGrey::ConvertPointGreyToOpenCV()
+{
+    unsigned int rowBytes = (double)rgbImage.GetReceivedDataSize()/(double)rgbImage.GetRows();
+    cv::Mat image = cv::Mat(rgbImage.GetRows(), rgbImage.GetCols(), CV_8UC3, rgbImage.GetData(),rowBytes);
+
+    return image;
+}
+
+cv::Mat OCVPointGrey::GetOpenCVFormat()
+{
+    RestoreCameraBuffer();
+    ConvertToRGB();
+    cv::Mat image = ConvertPointGreyToOpenCV();
+
+    return image;
+}
+
+
+
+
+
---
+++ apOCVPointGrey.hpp
@@ -0,0 +1,47 @@
+#ifndef APOCVPOINTGREY_H
+#define APOCVPOINTGREY_H
+
+#include "opencv2/core/core.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include <opencv2/opencv.hpp>
+#include "flycapture/FlyCapture2.h"
+
+#include <boost/shared_ptr.hpp>
+
+using namespace FlyCapture2;
+using namespace boost;
+using namespace std;
+
+typedef shared_ptr<Camera> Camera_ptr;
+
+namespace ap {
+    class OCVPointGrey
+    {
+    public:
+        OCVPointGrey();
+        ~OCVPointGrey();
+        bool OpenCamera();
+        bool RestoreCameraBuffer();
+        cv::Mat ConvertPointGreyToOpenCV();
+        void ConvertToRGB();
+        cv::Mat GetOpenCVFormat();
+
+
+    private:
+        bool GetCamerasNumber(unsigned int num, BusManager *bus_mgr);
+        bool GetCamera( BusManager *bus_mgr);
+        bool CameraConnect();
+        bool GetCameraInfo(CameraInfo camInfo, bool show);
+        bool StartCameraCapture();
+        bool StopCameraCapture();
+
+    private:
+        Camera_ptr camera;
+        PGRGuid guid;
+        Image rawImage;
+        Image rgbImage;
+
+    };
+}
+
+#endif // APOCVPOINTGREY_H
